edef22dc05a7861cf2939c3498b1320ca4cc196d
Mizael Nazareno <mizael.nazareno@AC-LPT-00857.local>
2021-09-08 13:40:14 -0300

    update cenários

--- cypress/integration/1-Jeep/monteseucarro.js
+++ cypress/integration/1-Jeep/monteseucarro.js
@@ -4,7 +4,6 @@
 describe('Acessa NMSC', () => {
   before(() => {
     cy.visit('/')
-    cy.wait(5000)
   });
 
   it('Seleciono localização e versão', () => {
@@ -17,16 +16,33 @@ describe('Acessa NMSC', () => {
     cy.get('.styles__Container-sc-168ooz8-0 > button').click( {force: true} )
   });
 
-
   it('Seleciono cor', () => {
     cy.wait(4000)
     cy.get(':nth-child(4) > .styles__Box-sc-cdg6y1-3 > .styles__Check-sc-cdg6y1-1 > img').click( {force: true} )
     cy.contains('AZUL JAZZ').click()
-});
+  });
+
+  it('Escolho opcionais', () => {
+    cy.get('.styles__Container-sc-168ooz8-0 > button').click( {force: true} )
+    cy.get(':nth-child(2) > .styles__Box-sc-cdg6y1-3 > .styles__Check-sc-cdg6y1-1 > img').click( {force: true} )
+  });
+
+  it('Escolho acessórios', () => {
+    cy.get('.styles__Container-sc-168ooz8-0 > button').click( {force: true} )
+    cy.get(':nth-child(4) > .styles__Container-sc-cdg6y1-0 > .styles__Box-sc-cdg6y1-3 > .styles__Check-sc-cdg6y1-1 > img')
+    .click( {force: true} )
+    cy.get(':nth-child(6) > .styles__Container-sc-cdg6y1-0 > .styles__Box-sc-cdg6y1-3 > .styles__Check-sc-cdg6y1-1 > img')
+    .click( {force: true} )
+  });
 
 
 
 
+  
+  it('Clico em resumo', () => {
+    cy.get('.styles__Container-sc-1nd1h4r-0 > button').click( {force: true} )
+
+  });
 
 
 
