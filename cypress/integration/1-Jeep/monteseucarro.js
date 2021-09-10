c5bd7e946dadef43e58bfccaf080fa5343a4ad78
Mizael Nazareno <mizael.nazareno@AC-LPT-00857.local>
2021-09-10 15:30:05 -0300

    update cenários

--- cypress/integration/1-Jeep/monteseucarro.js
+++ cypress/integration/1-Jeep/monteseucarro.js
@@ -18,7 +18,7 @@ describe('Acessa NMSC', () => {
   });
 
   it('Cor', () => {
-    cy.wait(4000)
+    cy.wait(3000)
     cy.get(':nth-child(4) > .styles__Box-sc-cdg6y1-3 > .styles__Check-sc-cdg6y1-1 > img').click( {force: true} )
     cy.contains('PRETO CARBON').click()
   });
@@ -40,34 +40,29 @@ describe('Acessa NMSC', () => {
     cy.get('.styles__Container-sc-1nd1h4r-0 > button').click( {force: true} )
   });
 
-
-
-
   it('Formulário Lead', () => {
     cy.get('.styles__Container-sc-1nd1h4r-0 > button').click( {force: true} )
-
-
     cy.get('#nome').as('nome').type('Mizael Tobias Nazareno')
-    cy.get('#e-mail').as('email').type('mizael.nazareno@avenuecode.com')
+    cy.get('#e-mail').as('email').type('faker@faker.com')
     cy.get('#telefone').as('numerotelefone').type('21333333333')
-    //cy.get('#person-type')
+    cy.get('#person-type').as('tipoPessoa').select('Pessoa Física')
+    cy.get('input[name="cpf"]').as('cpf').type('12345678900')
+    cy.get('.styles__ScrollBody-sc-1tamnkh-2').scrollIntoView()
+    cy.get('@nome').should('be.empty')
+    cy.get('@email').should('be.empty')
+    cy.get('@numerotelefone').should('be.empty')
+    cy.get('@cpf').should('be.empty')
+
+    cy.get('.styles__ButtonUserLocation-sc-1hvs87e-4').click( {force: true} )
+    cy.wait(2000)
+    cy.get('.bVndAO > input').click( {force: true} ).scrollIntoView()
+    cy.get('.styles__ButtonSend-sc-1hvs87e-14').click( {force: true} )
 
 
 
-   // cy.contains('Pessoa Física').click( {force: true} )
-
-
-
-
-   // cy.get('#cpf\/cnpj').as('cpf\cnpj').type('12345678900')
-    //cy.get('.styles__ButtonUserLocation-sc-1hvs87e-4').click( {force: true} )
-    //cy.wait(2000)
-    //cy.get('.bVndAO > input').click( {force: true} )
-    //cy.get('.styles__ButtonSend-sc-1hvs87e-14').click( {force: true} )
-
-
-
-
+    cy.contains('SUA PROPOSTA FOI ENVIADA').should('have.text','SUA PROPOSTA FOI ENVIADA')
+    
+    
 
 
 
