f7c06ff2d89eb3bee1df93034ab57d3def9db039
Mizael T. S. Nazareno <mtszareno@gmail.com>
2021-09-20 22:38:54 -0300

    Refatorando cenários

--- cypress/integration/1-Jeep/monteseucarro.js
+++ cypress/integration/1-Jeep/monteseucarro.js
@@ -4,7 +4,7 @@
 describe('Acessa NMSC', () => {
   before(() => {
     cy.visit('/')
-    cy.reload()
+    //cy.reload()
   });
 
   it('Seleciono localização e versão', () => {
@@ -12,13 +12,13 @@ describe('Acessa NMSC', () => {
     cy.get('input').type('Rio de Janeiro')
     cy.contains('Rio de Janeiro - RJ').click()
     cy.get('.styles__Option-sc-5vzz9o-6').click( {force: true} )
-    cy.get('.styles__PrimaryButton-sc-5vzz9o-0').click( {force: true} )
+    cy.contains('Continuar').click( {force: true} )
     cy.get(':nth-child(3) > .styles__Box-sc-cdg6y1-3 > .styles__Check-sc-cdg6y1-1 > img').click( {force: true} )
     cy.get('.styles__Container-sc-168ooz8-0 > button').click( {force: true} )
   });
 
   it('Cor', () => {
-    cy.wait(3000)
+    cy.wait(4000)
     cy.get(':nth-child(4) > .styles__Box-sc-cdg6y1-3 > .styles__Check-sc-cdg6y1-1 > img').click( {force: true} )
     cy.contains('PRETO CARBON').click()
   });
