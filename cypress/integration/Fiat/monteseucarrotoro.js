ddb2aa1d42503bebb568da52384f4ffb113a7605
Mizael Nazareno <mizael.nazareno@AC-LPT-00857.local>
2021-09-27 18:27:14 -0300

    cenários Toro

--- cypress/integration/Fiat/monteseucarrotoro.js
+++ cypress/integration/Fiat/monteseucarrotoro.js
@@ -35,5 +35,33 @@ describe('Acessa NMSC', () => {
     cy.get('.styles__Container-sc-xn9m9t-0 > button').click( {force: true} )
   });
 
+  it('Adicionais no Resumo', () => {
+    cy.get(':nth-child(11) > .styles__Box-sc-9sud6j-3').click( {force: true} )
+    cy.get(':nth-child(1) > .styles__Container-sc-1s904ag-0 > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img')
+    .click( {force: true} )
+    cy.get('.gFqBIx').click( {force: true} )
+    cy.get(':nth-child(2) > .styles__Container-sc-1s904ag-0 > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img')
+    .click( {force: true} )
+    cy.get('button > p').click( {force: true} )
+  });
 
+
+
+    it('Formulário Lead', () => {
+      cy.get('.styles__Container-sc-xn9m9t-0 > button').click( {force: true} )
+      cy.get('#nome').as('nome').type('Mizael Tobias Nazareno')
+      cy.get('#e-mail').as('email').type('faker@faker.com')
+      cy.get('#telefone').as('numerotelefone').type('21777777777')
+      cy.get('#person-type').as('tipoPessoa').select('Pessoa Física')
+      cy.get('input[name="cpf"]').as('cpf').type('00987654321')
+      cy.get('.styles__ScrollBody-sc-1tamnkh-2')
+
+      
+      cy.get('@nome').should('be.empty')
+      cy.get('@email').should('be.empty')
+      cy.get('@numerotelefone').should('be.empty')
+      cy.get('@cpf').should('be.empty')
+  
+
+    });
   });
