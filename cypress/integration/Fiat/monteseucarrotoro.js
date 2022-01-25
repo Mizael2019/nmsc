b9327ace91110f33109c2115264dfcdbf6084fba
Mizael Nazareno <mtszareno@gmail.com>
2022-01-25 01:33:28 -0300

    Ajustes nmsc

--- cypress/integration/Fiat/monteseucarrotoro.js
+++ cypress/integration/Fiat/monteseucarrotoro.js
@@ -6,55 +6,54 @@ describe('Acessa NMSC', () => {
   });
 
   it('Seleciono localização e versão', () => {
-    cy.get('.style__FixedButton-sc-9ihi4h-2 > button').click( {force: true} )
-    cy.get('input').type('Rio de Janeiro')
-    cy.get('.styles__PrimaryButton-sc-fj3yk2-0').click( {force: true} ).scrollIntoView()
-    cy.contains('Rio de Janeiro - RJ').click()
-    cy.get(':nth-child(3) > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img').click( {force: true} )
-    cy.get('.styles__Container-sc-y1ybxp-0 > button').click( {force: true} )
-  });
+    cy.get(':nth-child(2) > :nth-child(1) > .style__FixedButton-sc-9ihi4h-2 > button')
+    .click({force: true})
+    cy.get('.naGIG > :nth-child(1) > .style__FixedButton-sc-9ihi4h-2 > button')
+    .click({force: true})
+    cy.get('input[name="city"]').type('Rio de Janeiro').click({force: true})
+    cy.get('.styles__Option-sc-fj3yk2-6').click({force: true})
+    cy.get('.styles__PrimaryButton-sc-fj3yk2-0').click({force: true})
+    cy.get('[alt="FREEDOM TURBO 270 FLEX AT6"]').click({force: true}) 
+});
+ 
 
   it('Cor', () => {
     cy.wait(4000)
-    cy.get(':nth-child(3) > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img').click( {force: true} )
-    cy.contains('PRATA BILLET').click()
+    cy.get('.styles__Container-sc-y1ybxp-0 > button').click({force: true})
+    cy.get(':nth-child(3) > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img')
+    .click( {force: true} )
+    cy.get('[alt="PRATA BILLET"]').click({force: true})
   });
+
 
   it('Kit Opcionais', () => {
     cy.get('.styles__Container-sc-y1ybxp-0 > button').click( {force: true} )
-    cy.contains('Ops. A versão que você escolheu não tem opcionais. Pode pular essa etapa.').focused
+    cy.get('.styles__Infos-sc-1s904ag-4 > div').click( {force: true} )
     cy.get('.styles__Container-sc-y1ybxp-0 > button').click( {force: true} )
   });
 
+
   it('Acessórios', () => {
-    cy.get(':nth-child(1) > .styles__Container-sc-1s904ag-0 > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img')
-    .click( {force: true})
+    cy.get('[alt="ADESIVO LATERAL DE CAÇAMBA"]').click( {force: true})
+    cy.get('.styles__Container-sc-xn9m9t-0 > button').click( {force: true} )
   });
+
 
   it('Resumo', () => {
     cy.get('.styles__Container-sc-xn9m9t-0 > button').click( {force: true} )
   });
 
-  it('Adicionais no Resumo', () => {
-    cy.get(':nth-child(11) > .styles__Box-sc-9sud6j-3').click( {force: true} )
-    cy.get(':nth-child(1) > .styles__Container-sc-1s904ag-0 > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img')
-    .click( {force: true} )
-    cy.get('.gFqBIx').click( {force: true} )
-    cy.get(':nth-child(2) > .styles__Container-sc-1s904ag-0 > .styles__Box-sc-1s904ag-3 > .styles__Check-sc-1s904ag-1 > img')
-    .click( {force: true} )
-    cy.get('button > p').click( {force: true} )
-  });
-
     it('Formulário Lead', () => {
       cy.get('.styles__Container-sc-xn9m9t-0 > button').click( {force: true} )
-      cy.get('#nome').as('nome').type('Mizael Tobias Nazareno')
-      cy.get('#e-mail').as('email').type('faker@faker.com')
+      cy.get('#nome').as('nome').type('Tester User')
+      cy.get('#e-mail').as('email').type('test@fake.com')
       cy.get('#telefone').as('numerotelefone').type('21777777777')
       cy.get('#person-type').as('tipoPessoa').select('Pessoa Física')
       cy.get('input[name="cpf"]').as('cpf').type('00987654321')
       cy.get('.styles__ButtonUserLocation-sc-1l1w0zh-6').click( {force: true} )
       cy.get(':nth-child(3) > [type="radio"]').click( {force: true} )
       cy.get('.styles__ButtonSend-sc-1l1w0zh-15').click( {force: true} )
-      cy.contains('SUA PROPOSTA FOI ENVIADA').should('have.text','SUA PROPOSTA FOI ENVIADA')
+      cy.contains('Você receberá um e-mail/whatsapp com o resumo do carro que você montou.')
+      .should('have.text','Você receberá um e-mail/whatsapp com o resumo do carro que você montou.')
     });
   });
